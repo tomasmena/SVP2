91cd60fb383cbc612d8cc50631b68307fd896a4f
Tomas_Mena <tomasmena@gmail.com>
2023-10-18 16:23:57 +0200

    Boundary geojson

--- mapPolygons.js
+++ mapPolygons.js
@@ -1,7 +1,7 @@
 //import data from '../assets/midpoints.json' assert {type:'JSON'}
 
 async function polygons(){
-    const response = fetch('./assets/Intactforest4_Tomas.geojson');
+    const response = fetch('./assets/180kmboundary-Tomas.geojson');
     return (await response).json()
 };
 export const mapPolygons =  await polygons();
