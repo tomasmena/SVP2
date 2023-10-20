c4842c91464f092d4b65391dee43919d4e1c2ae3
Tomas_Mena <tomasmena@gmail.com>
2023-10-20 15:59:49 +0200

    Overlapping jsons

--- mapPolygons.js
+++ mapPolygons.js
@@ -1,8 +1,33 @@
 //import data from '../assets/midpoints.json' assert {type:'JSON'}
 
-async function polygons(){
-    const response = fetch('./assets/180kmboundary-Tomas.geojson');
-    return (await response).json()
-};
-export const mapPolygons =  await polygons();
 
+
+const fileList=['180kmboundary-Tomas_line.geojson','Arcticline_Tomas_line.geojson','Intactforest4_Tomas_pol.geojson']
+
+
+async function polygons(files){
+    const response = fetch(`./assets/${files}`);
+    return (await response).json()
+
+};
+
+
+const allCoordPol=[]
+const allCoordLine=[]
+
+for(let file of fileList){
+    if (file.includes('_line')){
+        let lineFile=file;
+        const coord =  await polygons(file);
+        allCoordLine.push(coord);
+    }
+    else{
+        let polFile=file;
+        const coord =  await polygons(file);
+        allCoordPol.push(coord);
+    }
+    
+}
+
+export const mapLines = allCoordLine
+export const mapPolygons = allCoordPol
